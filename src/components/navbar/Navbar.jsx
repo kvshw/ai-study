fffe3529d052754b765617a46a6da6bde6fb4487
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2023-02-01 13:16:15 +0200

    header-section addded

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,8 +7,8 @@ import logo from '../../assets/logo.svg'
 const Menu = () => (
     <>
         <p><a href='#home'>Home</a></p>
-        <p><a href='#wgpt3'>What is GPT-3</a></p>
-        <p><a href='#possibility'>Open AI</a></p>
+        <p><a href='#wgpt3'>What is VisionForge</a></p>
+        <p><a href='#possibility'>Styragon</a></p>
         <p><a href='#features'>Case Study</a></p>
         <p><a href='#blog'>Library</a></p>
     </>
