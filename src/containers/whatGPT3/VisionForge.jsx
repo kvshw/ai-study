c26f0c80dd55749f4111e440ec71c04c171eede9
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2023-02-03 11:26:32 +0200

    navigation error solved

--- src/containers/whatGPT3/VisionForge.jsx
+++ src/containers/whatGPT3/VisionForge.jsx
@@ -3,7 +3,7 @@ import Feature from '../../components/feature/Feature';
 import './visionForge.css';
 
 const VisionForge = () => (
-    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+    <div className="gpt3__whatgpt3 section__margin" id="vision">
         <div className="gpt3__whatgpt3-feature">
             <Feature title="What is VisionForge" text="VisionForge AI is a leading artificial intelligence company that provides businesses with innovative technology solutions to improve their operations, enhance customer experiences, and drive growth. With a focus on cutting-edge AI technologies, such as machine learning and deep neural networks, VisionForge AI helps organizations tackle complex challenges and make informed decisions." />
         </div>
